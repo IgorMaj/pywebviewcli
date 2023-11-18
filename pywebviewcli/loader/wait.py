e7933ab34e7913bbe4ee26b01efad2bd77c54667
IgorMaj <majic753@gmail.com>
2023-11-18 11:20:02 +0100

    Logging added

--- pywebviewcli/loader/wait.py
+++ pywebviewcli/loader/wait.py
@@ -1,12 +1,16 @@
+import logging
 from http_manager.parse_url import parse_hostname_and_port
 from libs.wait_for_tcp_port import wait_for_port
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("pywebviewclistartup")
+
 
 def wait_for_server_startup(url: str, timeout: int):
-    print(f"Waiting for {url} to load...")
+    logger.info(f"Waiting for {url} to load...")
     try:
         hostname, port = parse_hostname_and_port(url)
         wait_for_port(port=port, host=hostname, timeout=timeout)
     except:
         pass
-    print(f"Done waiting for {url}  to load...")
+    logger.info(f"Done waiting for {url}  to load...")
