93f99a2910bff78bc4895b22206cb3a73f09559f
IgorMaj <majic753@gmail.com>
2023-12-13 19:44:55 +0100

    Tests + cleanup

---
+++ tests/commands/test_build.py
@@ -0,0 +1,33 @@
+import sys
+import pathlib
+
+# to be able to see args and other modules which build_module relies on
+sys.path.insert(0, str(pathlib.Path("../src/pywebviewcli").absolute()))
+import src.pywebviewcli.commands.build as build_module
+
+
+def test_generate_app_template():
+    template_str = build_module.generate_app_template()
+    assert '"App"' in template_str
+    assert "static/" in template_str
+
+
+def test_generate_app_template_api_path():
+    template_str = build_module.generate_app_template(
+        api_path="./api/example.py",
+    )
+    assert "import example" in template_str
+
+
+def test_generate_app_template_title():
+    template_str = build_module.generate_app_template(
+        app_title="Custom Title",
+    )
+    assert '"Custom Title"' in template_str
+
+
+def test_generate_app_template_static_dirname():
+    template_str = build_module.generate_app_template(
+        static_dirname="static_custom",
+    )
+    assert "static_custom/" in template_str
