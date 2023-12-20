33bfe7408bd2271140961e65a9df1c081c9ec6bd
IgorMaj <majic753@gmail.com>
2023-12-20 19:56:46 +0100

    Test added

--- tests/commands/test_build.py
+++ tests/commands/test_build.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from unittest import mock
 
 # to be able to see args and other modules which build_module relies on
 sys.path.insert(
@@ -7,6 +8,14 @@ sys.path.insert(
     str(Path(f"{Path(__file__).parent}/../../src/pywebviewcli").absolute()),
 )
 import src.pywebviewcli.commands.build as build_module
+
+
+@mock.patch("os.chdir")
+@mock.patch("os.system")
+def test_package_app(mock_system, mock_chdir):
+    build_module.package_app("/tmp/test1")
+    mock_chdir.assert_called_with("/tmp/test1")
+    mock_system.assert_called()
 
 
 def test_generate_app_template():
