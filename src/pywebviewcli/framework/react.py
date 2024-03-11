ea1ecc98c8a7474cfb2b52a427496ec620a10e14
IgorMaj <majic753@gmail.com>
2024-03-11 17:35:40 +0100

    Tests and refactoring

--- src/pywebviewcli/framework/react.py
+++ src/pywebviewcli/framework/react.py
@@ -1,4 +1,5 @@
 from file_manager.methods import file_exists, read_env_file, write_env_file
+from .common import parse_app_init_file
 from templates.generate import generate_init_template
 
 
@@ -29,29 +30,6 @@ def patch_react_env_file():
 
     # .env file doesn't exist. Init it.
     write_env_file(".env", [f"{browser_cmd_template}\n"])
-
-
-def parse_app_init_file(file_path: str):
-    """Returns imports and the rest as strings, used by the template"""
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-
-    import_lines = []
-    main_code_lines = []
-
-    # Iterate through the lines of the file
-    for line in lines:
-        if line.startswith("import"):
-            import_lines.append(line)
-        else:
-            # we indent, since it will end up inside the add event listener callback, for format reasons
-            main_code_lines.append(f"\t{line}")
-
-    # Join the lines into multiline strings, note: lines already contain newline separator
-    imports = "".join(import_lines)
-    main_code = "".join(main_code_lines)
-
-    return imports, main_code
 
 
 def react_action(project_dir_path: str):
